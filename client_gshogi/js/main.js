d2d6db41e5d3c3741e5021124e66e6d8206822d3
Momose Ryoya <au09084327117@gmail.com>
2017-05-17 15:38:32 +0000

    司令塔に入る処理周りを追加

--- client_gshogi/js/main.js
+++ client_gshogi/js/main.js
@@ -389,10 +389,27 @@ class normalMan{
 			if(this._y==5 && _y==4) return false;
 		}
 		
-		// 司令塔の部分は1じゃなくても動ける可能性あり
-		// if((x=)){
-			
-		// }
+		// 自分の司令塔にいる場合
+		if(this._x==(FIELD_NONE_BY_SHIRE-1) && (this._y==FIELD_Y)){
+			if(this._y == _y && _x==FIELD_NONE_BY_SHIRE+1) return true;
+			if((_y==FIELD_Y-1) && _x==FIELD_NONE_BY_SHIRE) return true;
+		}
+		// 相手の司令塔にいる場合
+		if(this._x==(FIELD_NONE_BY_SHIRE-1) && (this._y==1)){
+			if(this._y == _y && _x==FIELD_NONE_BY_SHIRE+1) return true;
+			if((_y==2) && _x==FIELD_NONE_BY_SHIRE) return true;
+		}
+		
+		// 自分の司令塔にはいるとき
+		if((_x == FIELD_NONE_BY_SHIRE-1) && (_y == FIELD_Y)){
+			if(this._x == FIELD_NONE_BY_SHIRE && this._y == FIELD_Y-1) return true;
+			if(this._x == FIELD_NONE_BY_SHIRE+1 && this._y == FIELD_Y) return true;
+		}
+		// 相手の司令塔に入るとき
+		if((_x == FIELD_NONE_BY_SHIRE-1) && (_y == 1)){
+			if(this._x == FIELD_NONE_BY_SHIRE && this._y == 2) return true;
+			if(this._x == FIELD_NONE_BY_SHIRE+1 && this._y == 1) return true;
+		}
 		
 		if(Math.abs(this._x - _x) + Math.abs(this._y - _y) == 1) return true;
 		else return false;
@@ -425,16 +442,26 @@ class airMan{
 		
 		if(this._x==_x) return true;
 		
-		// 司令塔にいる場合
-		if(this._x==3 && this._y==8){
-			if(this._x==4 && this._y!=1) return true;
+		// 自分の司令塔にいる場合
+		if(this._x==(FIELD_NONE_BY_SHIRE-1) && (this._y==FIELD_Y)){
+			if(this._y == _y && _x==FIELD_NONE_BY_SHIRE+1) return true;
+			if(_x==FIELD_NONE_BY_SHIRE) return true;
+		}
+		// 相手の司令塔にいる場合
+		if(this._x==(FIELD_NONE_BY_SHIRE-1) && (this._y==1)){
+			if(this._y == _y && _x==FIELD_NONE_BY_SHIRE+1) return true;
+			if(_x==FIELD_NONE_BY_SHIRE) return true;
 		}
 		
-		//司令塔に入るとき
-		if(this._x==3 || this._x==4){
-			if((_y==1 || _y==8) && (_x==3)){
-				return true;
-			}
+		// 自分の司令塔にはいるとき
+		if((_x == FIELD_NONE_BY_SHIRE-1) && (_y == FIELD_Y)){
+			if(this._x == FIELD_NONE_BY_SHIRE) return true;
+			if(this._x == FIELD_NONE_BY_SHIRE+1 && this._y == FIELD_Y) return true;
+		}
+		// 相手の司令塔に入るとき
+		if((_x == FIELD_NONE_BY_SHIRE-1) && (_y == 1)){
+			if(this._x == FIELD_NONE_BY_SHIRE) return true;
+			if(this._x == FIELD_NONE_BY_SHIRE+1 && this._y == 1) return true;
 		}
 		
 		// 横方向
@@ -471,6 +498,28 @@ class tankMan{
 		if(!(this._x==2 || this._x==5)) {
 			if(this._y==4 && _y==5) return false;
 			if(this._y==5 && _y==4) return false;
+		}
+		
+		// 自分の司令塔にいる場合
+		if(this._x==(FIELD_NONE_BY_SHIRE-1) && (this._y==FIELD_Y)){
+			if(this._y == _y && _x==FIELD_NONE_BY_SHIRE+1) return true;
+			if((_y==FIELD_Y-1) && _x==FIELD_NONE_BY_SHIRE) return true;
+		}
+		// 相手の司令塔にいる場合
+		if(this._x==(FIELD_NONE_BY_SHIRE-1) && (this._y==1)){
+			if(this._y == _y && _x==FIELD_NONE_BY_SHIRE+1) return true;
+			if((_y==2) && _x==FIELD_NONE_BY_SHIRE) return true;
+		}
+		
+		// 自分の司令塔にはいるとき
+		if((_x == FIELD_NONE_BY_SHIRE-1) && (_y == FIELD_Y)){
+			if(this._x == FIELD_NONE_BY_SHIRE && this._y == FIELD_Y-1) return true;
+			if(this._x == FIELD_NONE_BY_SHIRE+1 && this._y == FIELD_Y) return true;
+		}
+		// 相手の司令塔に入るとき
+		if((_x == FIELD_NONE_BY_SHIRE-1) && (_y == 1)){
+			if(this._x == FIELD_NONE_BY_SHIRE && this._y <= 3) return true;
+			if(this._x == FIELD_NONE_BY_SHIRE+1 && this._y == 1) return true;
 		}
 		
 		// 周囲1マス
@@ -515,6 +564,28 @@ class koheiMan{
 		if(!(this._x==2 || this._x==5)) {
 			if(this._y<=4 && _y>=5) return false;
 			if(this._y>=5 && _y<=4) return false;
+		}
+		
+		// 自分の司令塔にいる場合
+		if(this._x==(FIELD_NONE_BY_SHIRE-1) && (this._y==FIELD_Y)){
+			if(this._y == _y && _x==FIELD_NONE_BY_SHIRE+1) return true;
+			if(_x==FIELD_NONE_BY_SHIRE) return true;
+		}
+		// 相手の司令塔にいる場合
+		if(this._x==(FIELD_NONE_BY_SHIRE-1) && (this._y==1)){
+			if(this._y == _y && _x==FIELD_NONE_BY_SHIRE+1) return true;
+			if(_x==FIELD_NONE_BY_SHIRE) return true;
+		}
+		
+		// 自分の司令塔にはいるとき
+		if((_x == FIELD_NONE_BY_SHIRE-1) && (_y == FIELD_Y)){
+			if(this._x == FIELD_NONE_BY_SHIRE) return true;
+			if(this._x == FIELD_NONE_BY_SHIRE+1 && this._y == FIELD_Y) return true;
+		}
+		// 相手の司令塔に入るとき
+		if((_x == FIELD_NONE_BY_SHIRE-1) && (_y == 1)){
+			if(this._x == FIELD_NONE_BY_SHIRE) return true;
+			if(this._x == FIELD_NONE_BY_SHIRE+1 && this._y == 1) return true;
 		}
 		
 		if(this._x==_x || this._y==_y) return true;
