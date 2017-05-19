0fc7cd3f02c5e29873cf76dc11aff2eeda0f6aa5
Momose Ryoya <au09084327117@gmail.com>
2017-05-19 22:44:44 +0000

    server先を一時変更

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var router = express();
 var server = http.createServer(router);
 var io = socketio.listen(server);
 
-router.use(express.static(path.resolve(__dirname, 'client')));
+router.use(express.static(path.resolve(__dirname, 'client_gshogi')));
 var messages = [];
 var sockets = [];
 
